784d2569459af135015b4ee6e463dd025a8e20bd
Poxie <poxen12@hotmail.com>
2021-07-18 08:30:46 +0200

    Added tooltips to the control buttons

--- frontend/src/pages/room/ControlButton.tsx
+++ frontend/src/pages/room/ControlButton.tsx
@@ -1,18 +1,22 @@
 import { Clickable } from "../../components/Clickable"
 import { Flex } from "../../components/Flex"
+import { Tooltip } from "../../components/Tooltip"
 
 interface Props {
     onClick: () => void;
     active: boolean;
     children: any;
+    tooltip: string;
 }
 
-export const ControlButton: React.FC<Props> = ({ onClick, active, children }) => {
+export const ControlButton: React.FC<Props> = ({ onClick, active, children, tooltip }) => {
     return(
         <Clickable className={`control-btn${active ? ' active-btn' : ''}`} onClick={onClick}>
-            <Flex alignItems={'center'} style={{height: '100%'}} justifyContent={'center'}>
-                {children}
-            </Flex>
+            <Tooltip tooltip={tooltip} style={{height: '100%'}}>
+                <Flex alignItems={'center'} style={{height: '100%'}} justifyContent={'center'}>
+                    {children}
+                </Flex>
+            </Tooltip>
         </Clickable>
     )
 }
