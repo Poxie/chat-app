05cec8866ec282e8b6efa5827e392a547da18f40
Poxie <poxen12@hotmail.com>
2021-07-20 14:30:47 +0200

    Animation changes - moved to JS instead of flex-box

--- frontend/src/pages/room/RoomStreams.tsx
+++ frontend/src/pages/room/RoomStreams.tsx
@@ -1,26 +1,54 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Flex } from "../../components/Flex";
 import { useRoom } from "../../contexts/RoomProvider"
 import { Stream } from "./Stream";
 
 export const RoomStreams = () => {
-    const { streams } = useRoom();
+    let { streams } = useRoom();
+    const ref = useRef<null | HTMLDivElement>(null);
+
+    useEffect(() => {
+        if(!ref.current) return;
+        // if(streams.length === 3 || streams.length === 4) {
+        //     ref.current.style.width = `1000px`;
+        //     document.documentElement.style.setProperty('--row-items', '2');
+        // }
+        // if(streams.length < 3) {
+        //     document.documentElement.style.setProperty('--row-items', '2');
+        //     ref.current.style.width = `100%`;
+        // } else if(streams.length >= 5) {
+        //     document.documentElement.style.setProperty('--row-items', '3');
+        //     ref.current.style.width = `unset`;
+        // }
+    }, [streams]);
+
+    // let newS: any = [];
+    // streams.forEach(stream => newS.push(stream));
+    // streams.forEach(stream => newS.push(stream));
+    // streams.forEach(stream => newS.push(stream));
+    // streams.forEach(stream => newS.push(stream));
+    // streams = [...streams, ...newS];
 
     return(
-        <Flex className="room-streams" flexWrap={'wrap'} justifyContent={'center'}>
-            {streams.map((stream, key) => {
-                return(
-                    <Stream 
-                        stream={stream.stream}
-                        user={stream.user}
-                        isMuted={stream.isMuted}
-                        hasCamera={stream.hasCamera}
-                        disconnected={stream.disconnected}
-                        connecting={stream.connecting}
-                        key={stream.stream.id}
-                    />
-                )
-            })}
+        <Flex className="streams" flexWrap={'wrap'} justifyContent={'center'}>
+            <div className="room-streams" style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}} ref={ref}>
+                {streams.map((stream, key) => {
+                    return(
+                        <Stream 
+                            stream={stream.stream}
+                            user={stream.user}
+                            isMuted={stream.isMuted}
+                            hasCamera={stream.hasCamera}
+                            disconnected={stream.disconnected}
+                            connecting={stream.connecting}
+                            streamAmount={streams.length}
+                            orderId={key}
+                            container={ref}
+                            key={Math.random()}
+                        />
+                    )
+                })}
+            </div>
         </Flex>
     )
 }
--- frontend/src/pages/room/Stream.tsx
+++ frontend/src/pages/room/Stream.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { Flex } from "../../components/Flex";
 import { User } from "../../types/User";
 import { IsMutedIcon } from "./IsMutedIcon";
@@ -11,12 +11,36 @@ interface Props {
     user: User;
     isMuted: boolean;
     hasCamera: boolean;
+    orderId?: number;
     disconnected?: boolean;
+    container?: any;
     isNavStream?: boolean;
     connecting?: boolean;
+    streamAmount?: number;
 }
 
-export const Stream: React.FC<Props> = ({ stream, user, hasCamera, isMuted, disconnected, connecting, isNavStream=false }) => {
+const chunkArray = (streamAmount: number, rowAmount: number) => {
+    var array: any = [];
+    for(let i = 0; i < streamAmount; i++) {
+        array.push(i);
+    }
+    return [].concat.apply([],
+        array.map(function(elem: any, i: number) {
+            return i % rowAmount ? [] : [array.slice(i, i + rowAmount)];
+        })
+    );
+}
+const ITEM_DIVIDERS: any = {
+    2: 2,
+    3: 1.57,
+    4: 1.31,
+    5: 1.22,
+    6: 1.091,
+    7: 1.05
+}
+const RATIO = 0.7492063492;
+const SPACING = 10;
+export const Stream: React.FC<Props> = ({ stream, user, hasCamera, isMuted, disconnected, connecting, orderId, container: streamContainer, isNavStream=false, streamAmount }) => {
     const { removeStream, setConnected } = useRoom();
     const ref = useRef<HTMLVideoElement | null>(null);
     const container = useRef<HTMLDivElement | null>(null);
@@ -44,9 +68,7 @@ export const Stream: React.FC<Props> = ({ stream, user, hasCamera, isMuted, disc
         if(disconnected === true) {
             setTimeout(() => {
                 container.current?.classList.add('shrink');
-                setTimeout(() => {
-                    removeStream(user.id);
-                }, 1000);
+                removeStream(user.id);
             }, 400);
         }
     }, [disconnected]);
@@ -59,8 +81,73 @@ export const Stream: React.FC<Props> = ({ stream, user, hasCamera, isMuted, disc
         }
     }, [connecting]);
 
+    const checkIfWidthExceeds: any = (width: number, containerHeight: number, amountOfRows: number) => {
+        if(width * RATIO * amountOfRows > containerHeight) {
+            return checkIfWidthExceeds(width - 1, containerHeight, amountOfRows);
+        }
+        return width;
+    }
+    const determineWidth = (isEven: boolean, availableWidth: number, rowAmount: number, amountOfRows: number, containerHeight: number) => {
+        let width = isEven ? (availableWidth / rowAmount) : (availableWidth / rowAmount);
+        return checkIfWidthExceeds(width, containerHeight, amountOfRows);
+    }
+    const getRowItemLength = useMemo(() => (rowAmount: number, currentRow: number, streamAmount: number) => {
+        const rows = chunkArray(streamAmount, rowAmount);
+        // @ts-ignore
+        let itemLength = rows[currentRow]?.length;
+        return [itemLength, rows];
+    }, []);
+    const determineLeft = (width: number, containerWidth: number, rowIndex: number, orderId: number, rowAmount: number, currentRow: number) => {
+        if(!streamAmount) return;
+        let left;
+        if(orderId + 1 === streamAmount && rowIndex === 1) {
+            left = containerWidth / 2 - width / 2 - SPACING;
+        } else {
+            rowIndex--;
+            const [rowItemLength, rows] = getRowItemLength(rowAmount, currentRow, streamAmount);
+            console.log(rowItemLength)
+            left = (rowAmount * width) / (rowItemLength / (rowItemLength - 1)) - (rowIndex * width) - (SPACING * 2 * (rowIndex)) + ((containerWidth - width * rowAmount) / 2) + SPACING * (rowItemLength - 1) - SPACING;
+        }
+        return left;
+    }
+    const resizeStreams = () => {
+        if(!streamAmount || !streamContainer.current || !container.current || orderId === undefined) return;
+        // Calculating the amount of streams per row
+        const isEven = streamAmount % 2 === 0;
+        let rowAmount;
+        if(streamAmount / 2 === 1) {
+            rowAmount = 2;
+        } else {
+            rowAmount = Math.floor(streamAmount/5) * 1 + 2;
+        }
+        const amountOfRows = Math.round(streamAmount / rowAmount);
+
+        const containerWidth = streamContainer.current.offsetWidth;
+        const containerHeight = streamContainer.current.offsetHeight - ((SPACING * 2) * amountOfRows);
+        const availableWidth = containerWidth - (streamAmount * SPACING + SPACING * 6);
+
+        const style = container.current.style;
+        // Setting width of stream
+        const width = determineWidth(isEven, availableWidth, rowAmount, amountOfRows, containerHeight);
+        style.width = `${width}px`;
+        const currentRow = amountOfRows === 1 ? 0 : Math.floor((orderId / rowAmount));
+        console.log(amountOfRows, currentRow, rowAmount);
+        const rowIndex = currentRow !== 0 ? orderId - (rowAmount * currentRow - 1) : orderId + 1;
+        // Setting position of element
+        const left = determineLeft(width, containerWidth, rowIndex, orderId, rowAmount, currentRow);
+        style.left = `${left}px`;
+        let topCurrentRow = amountOfRows === 1 ? 0 : Math.floor(orderId / rowAmount)
+        let top = width * RATIO * topCurrentRow + SPACING * topCurrentRow * 2;
+        style.top = `${top}px`;
+    }
+    useEffect(() => {
+        window.addEventListener('resize', resizeStreams);
+        return () => window.removeEventListener('resize', resizeStreams);
+    }, [streamAmount]);
+    useEffect(resizeStreams, [streamAmount, orderId]);
+
     return(
-        <div className={`user${isSpeaking ? ' is-speaking' : ''}${disconnected ? ' disconnected' : ''}${connecting ? ' connecting' : ''}`} ref={container}>
+        <div data-order={orderId} className={`user${isSpeaking ? ' is-speaking' : ''}${disconnected ? ' disconnected' : ''}${connecting ? ' connecting' : ''}`} ref={container}>
             {!isNavStream && (
                 <div className="user-top">
                     {isMuted && <IsMutedIcon />}
