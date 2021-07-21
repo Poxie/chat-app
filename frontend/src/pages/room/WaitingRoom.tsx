ead247e41c75671bd251892a8b11e2286e7dcfe6
Poxie <poxen12@hotmail.com>
2021-07-21 08:33:54 +0200

    Added member counter for waiting room

--- frontend/src/pages/room/WaitingRoom.tsx
+++ frontend/src/pages/room/WaitingRoom.tsx
@@ -1,16 +1,31 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Button } from "../../components/Button";
 import { Clickable } from "../../components/Clickable";
 import { Flex } from "../../components/Flex";
 import { useRoom } from "../../contexts/RoomProvider";
+import { User } from "../../types/User";
 import { CameraButton } from "./CameraButton";
 import { ControlButton } from "./ControlButton";
 import { MicButton } from "./MicButton";
 import { Stream } from "./Stream";
 
 export const WaitingRoom = () => {
-    const { selfStream, hasCamera, isMuted, toggleCamera, toggleMute, joinRoom } = useRoom();
+    const { selfStream, hasCamera, isMuted, toggleCamera, toggleMute, joinRoom, socket, roomId } = useRoom();
+    const [members, setMembers] = useState<null | number>(null);
 
+    useEffect(() => {
+        socket.on('member-update', (members: number) => {
+            setMembers(members);
+        })
+        socket.emit('listen-to-room', roomId);
+
+        return () => {
+            socket.emit('stop-listening')
+            socket.off('member-update');
+        }
+    }, []);
+
+    const singular = members === 1;
     return(
         <Flex className="waiting-room" justifyContent={'center'}>
             <Flex className="waiting-container" alignItems={'center'} flexWrap={'wrap'}>
@@ -39,6 +54,11 @@ export const WaitingRoom = () => {
                     <span>
                         Ready to join the meeting?
                     </span>
+                    {members !== null && (
+                        <span className="silent">
+                            There {singular ? 'is' : 'are'} {members} {singular ? 'member' : 'members'} in this meeting
+                        </span>
+                    )}
                     <Button rounded={true} onClick={joinRoom}>
                         Join meeting
                     </Button>
