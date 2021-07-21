995f92306a0ad89e17e89f6da091990d3ca8b2e2
Poxie <poxen12@hotmail.com>
2021-07-21 08:34:17 +0200

    Socket endpoints for listening to rooms

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,22 @@ const io = require('socket.io')(server, {
     }
 });
 
+const rooms = {};
 io.on('connection', socket => {
     socket.on('join-room', ({ roomId, user, isMuted, hasCamera }) => {
         socket.join(roomId);
         socket.broadcast.to(roomId).emit('user-connected', {user, isMuted, hasCamera});
+        if(!rooms[roomId]) rooms[roomId] = {members: 0};
+        if(rooms[roomId]) rooms[roomId].members++;
 
         socket.on('disconnect', () => {
+            if(rooms[roomId]) rooms[roomId].members--;
             socket.broadcast.to(roomId).emit('user-disconnected', user);
         })
     })
     socket.on('leave-room', ({ roomId, user }) => {
         socket.broadcast.to(roomId).emit('user-disconnected', user);
+        if(rooms[roomId]) rooms[roomId].members--;
         socket.leave(roomId);
     })
     socket.on('toggle-mute', ({ roomId, isMuted, streamId }) => {
@@ -34,6 +39,14 @@ io.on('connection', socket => {
     socket.on('send-message', ({ roomId, content, author }) => {
         console.log('received message');
         io.in(roomId).emit('send-message', ({ content, author, date: Date.now() }));
+    })
+    socket.on('listen-to-room', roomId => {
+        const interval = setInterval(() => {
+            socket.emit('member-update', rooms[roomId]?.members || 0);
+        }, 1000);
+        socket.on('stop-listening', () => {
+            clearInterval(interval);
+        })
     })
 })
 
