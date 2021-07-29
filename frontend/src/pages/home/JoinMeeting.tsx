588e7493b4e74a0454da33df718c9b101cc15268
Poxie <poxen12@hotmail.com>
2021-07-29 18:26:45 +0200

    Padding and text changes

--- frontend/src/pages/home/JoinMeeting.tsx
+++ frontend/src/pages/home/JoinMeeting.tsx
@@ -20,10 +20,10 @@ export const JoinMeeting = () => {
                 onSubmit={joinMeeting}
             />
             <Button type={'secondary'} onClick={joinMeeting}>
-                <span>
-                    Join
-                </span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                <Flex alignItems={'center'} justifyContent={'center'}>
+                    <svg className="first-arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                    <svg className="second-arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                </Flex>
             </Button>
         </Flex>
     )
