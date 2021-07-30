b5048241f271ec7e7770541d3f790eca41d1c314
Poxie <poxen12@hotmail.com>
2021-07-30 23:00:04 +0200

    Added camera icon as a component

--- frontend/src/pages/room/CameraButton.tsx
+++ frontend/src/pages/room/CameraButton.tsx
@@ -1,3 +1,4 @@
+import { CameraIcon } from "../../icons/CameraIcon"
 import { ControlButton } from "./ControlButton"
 
 interface Props {
@@ -12,7 +13,7 @@ export const CameraButton: React.FC<Props> = ({ hasCamera, toggleCamera }) => {
             onClick={toggleCamera}
             tooltip={hasCamera ? 'Turn off' : 'Turn on'}
         >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 16c0 1.104-.896 2-2 2h-12c-1.104 0-2-.896-2-2v-8c0-1.104.896-2 2-2h12c1.104 0 2 .896 2 2v8zm8-10l-6 4.223v3.554l6 4.223v-12z"/></svg>
+            <CameraIcon />
         </ControlButton>
     )
 }
